2cf2e0ffda9bea79b0784f89b89f72202fda371b
Alexey Chernobaev <achernobaev@dev.rtsoft.ru>
2018-03-30 22:05:25 +0300

    about dialog text changed

--- src/analyze/gui/aboutdialog.cpp
+++ src/analyze/gui/aboutdialog.cpp
@@ -15,39 +15,44 @@ AboutDialog::AboutDialog(QWidget *parent) :
 
     setWindowTitle("About " + AboutData::DisplayName);
 
-    ui->textEdit->viewport()->setAutoFillBackground(false);
+    ui->textBrowser->viewport()->setAutoFillBackground(false);
 
-    ui->textEdit->setHtml(QString(
+    ui->textBrowser->setHtml(QString(
         "<h2>%1 v.%2</h2>" \
-        "<p>%3</p>")
-      .arg(AboutData::DisplayName).arg(AboutData::Version)
-      .arg(AboutData::ShortDescription)
-        +
+        "<p>%3.</p>")
+        .arg(AboutData::DisplayName).arg(AboutData::Version)
+        .arg(AboutData::ShortDescription) +
+        QString(
+        "<p>%1.</p>")
+        .arg(AboutData::CopyrightStatement) +
 #ifdef SAMSUNG_TIZEN_BRANCH
         QString(
-        "<p>Based on Heaptrack memory profiler created by Milian Wolff " \
-        "&lt;<a href=mailto:mail@milianw.de>mail@milianw.de</a>&gt;</p>") +
+        "<p>Based on <a href=http://milianw.de/tag/heaptrack>Heaptrack memory profiler</a> " \
+        "created by Milian Wolff on terms of " \
+        "<a href=https://www.gnu.org/licenses/lgpl-3.0.en.html>LGPL</a>.</p>") +
 #endif
         QString(
-        "<p>%4</p>")
-        .arg(AboutData::CopyrightStatement) +
-        QString(
-        "<p>GNU LESSER GENERAL PUBLIC LICENSE v.2.1</p>" \
-        "<p>The application is based in part on the work of the Qwt project " \
-        "(<a href=http://qwt.sf.net>qwt.sf.net</a>)</p>")
+        "<p>Uses <a href=https://www.qt.io>Qt framework</a> v.%1 libraries on terms of " \
+        "<a href=https://www.gnu.org/licenses/lgpl-3.0.en.html>LGPL</a>.</p>" \
+        "<p>Uses <a href=https://cgit.kde.org/threadweaver.git>ThreadWeaver library</a> " \
+        "on terms of <a href=https://www.gnu.org/licenses/lgpl-3.0.en.html>LGPL</a>.</p>" \
+        "<p>The application is based in part on the work of the " \
+        "<a href=http://qwt.sf.net>Qwt project</a> on terms of " \
+        "<a href=http://qwt.sourceforge.net/qwtlicense.html>Qwt License</a>.</p>")
+        .arg(QT_VERSION_STR)
 #ifdef WINDOWS
         + QString(
         "<p>Application icon (free for commercial use): Jack Cai " \
-        "(<a href=http://www.doublejdesign.co.uk>www.doublejdesign.co.uk</a>)</p>")
+        "(<a href=http://www.doublejdesign.co.uk>www.doublejdesign.co.uk</a>).</p>")
 #endif
     );
 
-    QFontMetrics fm(ui->textEdit->font());
-    QRect rect = fm.boundingRect("The application is based in part on the work of the Qwt project (qwt.sf.net)");
+    QFontMetrics fm(ui->textBrowser->font());
+    QRect rect = fm.boundingRect("The application is based in part on the work of the Qwt project on terms of");
     int m = ui->verticalLayout->margin();
     int h = ui->buttonBox->height();
-    int textWidth = (int)round(rect.width() * 1.03);
-    int textHeight = (int)round(rect.height() * 1.03 * 14);
+    int textWidth = (int)round(rect.width() * 1.05);
+    int textHeight = (int)round(rect.height() * 1.03 * 17);
     resize(std::max(420, 2 * m + textWidth), std::max(252, 2 * m + h + textHeight));
 }
 
