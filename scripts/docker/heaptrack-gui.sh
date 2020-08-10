c520fab2ef349a37c9fd8fafa14f8f87935251fa
Timur <t.mustafin@partner.samsung.com>
2020-08-10 17:20:51 +0300

    Show filename in GUI header
    Signed-off-by: Timur <t.mustafin@partner.samsung.com>

--- scripts/docker/heaptrack-gui.sh
+++ scripts/docker/heaptrack-gui.sh
@@ -3,15 +3,16 @@
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 RES_FILE=$1
+BASENAME=`basename "$RES_FILE"`
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
 docker run -it \
     --volume=$XSOCK:$XSOCK:rw \
     --volume=$XAUTH:$XAUTH:rw \
-    --volume=$RES_FILE:/res.gz:ro \
+    --volume=$RES_FILE:/$BASENAME:ro \
     --env="XAUTHORITY=${XAUTH}" \
     --env="DISPLAY" \
     --user="docker-user" \
     heaptrack:latest \
-    /heaptrack/build-x64/bin/heaptrack_gui ${@:2:2} /res.gz 2>gui_malloc.srderr
+    /heaptrack/build-x64/bin/heaptrack_gui ${@:2:2} /$BASENAME 2>gui_malloc.srderr
